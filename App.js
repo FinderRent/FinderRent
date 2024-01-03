e7f5fe7069807536221e4a11319c20498adae777
Maor <maorsa9@gmail.com>
2024-01-03 18:29:46 +0200

    added possibllity to changes fonts text fot the app

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { useCallback, useEffect, useState } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { I18nManager } from 'react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -6,6 +10,8 @@ import Toast from 'react-native-toast-message';
 import { UserContext, useUsers } from './context/UserContext';
 import { DarkModeProvider } from './context/DarkModeContext';
 import AuthStackScreens from './navigation/AuthStackScreens';
+
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,19 +23,49 @@ const queryClient = new QueryClient({
 });
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
   const { login, logout, userData } = useUsers();
   I18nManager.forceRTL(false);
 
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          varelaRound: require('./assets/fonts/VarelaRound-Regular.ttf'),
+        });
+      } catch (error) {
+        console.log.error();
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <DarkModeProvider>
-      <QueryClientProvider client={queryClient}>
-        <UserContext.Provider value={{ login, logout, userData }}>
-          <StatusBar style="dark" />
-          <AuthStackScreens />
-          {/* <DrawerScreens /> */}
-          <Toast />
-        </UserContext.Provider>
-      </QueryClientProvider>
-    </DarkModeProvider>
+    <SafeAreaProvider onLayout={onLayout}>
+      <DarkModeProvider>
+        <QueryClientProvider client={queryClient}>
+          <UserContext.Provider value={{ login, logout, userData }}>
+            <StatusBar style="dark" />
+            <AuthStackScreens />
+            {/* <DrawerScreens /> */}
+            <Toast />
+          </UserContext.Provider>
+        </QueryClientProvider>
+      </DarkModeProvider>
+    </SafeAreaProvider>
   );
 }
