9bbf4034cb505ea708da0e75350efd30eb07d2c3
Maor <maorsa9@gmail.com>
2024-01-08 11:16:15 +0200

    small bug fix

--- navigation/MainTabScreen.js
+++ navigation/MainTabScreen.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Platform, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; // Updated import
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   MD3LightTheme as DefaultTheme,
   MD3DarkTheme as DarkTheme,
   Provider as PaperProvider,
 } from 'react-native-paper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -59,121 +60,123 @@ function MainTabScreen() {
   }, [userData]);
 
   return (
-    <BottomSheetModalProvider>
-      <PaperProvider theme={theme}>
-        <Tab.Navigator
-          initialRouteName="HomeStackScreen"
-          screenOptions={{
-            headerShown: false,
-            tabBarHideOnKeyboard: true,
-            activeTintColor: isDarkMode ? Color.white : Color.black,
-            inactiveTintColor: isDarkMode ? Color.white : Color.black,
-            tabBarStyle: {
-              backgroundColor: isDarkMode ? Color.darkTheme : Color.white,
-              borderTopColor: Color.Brown100,
-              borderTopWidth: 1,
-              height: Platform.OS === 'ios' ? '9.5%' : '7%',
-              position: 'absolute',
-              padding: Platform.OS === 'ios' ? '6%' : '6%',
-            },
-          }}
-        >
-          <Tab.Screen
-            name="HomeStackScreen"
-            component={HomeStackScreen}
-            options={{
-              tabBarLabel: '',
-              tabBarIcon: ({ focused }) => (
-                <View style={{ alignItems: 'center' }}>
-                  <MaterialCommunityIcons
-                    name={focused ? 'home' : 'home-outline'}
-                    style={{ marginTop: -10 }}
-                    color={isDarkMode ? Color.white : Color.darkTheme}
-                    size={26}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 10,
-                      color: isDarkMode ? Color.white : Color.black,
-                    }}
-                  >
-                    Home
-                  </Text>
-                </View>
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="ProfileStackScreen"
-            component={ProfileStackScreen}
-            initialParams={{ isAuthenticated }}
-            listeners={({ navigation }) => ({
-              tabPress: (e) => {
-                e.preventDefault();
-                navigation.navigate('ProfileStackScreen', {
-                  isAuthenticated,
-                });
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <PaperProvider theme={theme}>
+          <Tab.Navigator
+            initialRouteName="HomeStackScreen"
+            screenOptions={{
+              headerShown: false,
+              tabBarHideOnKeyboard: true,
+              activeTintColor: isDarkMode ? Color.white : Color.black,
+              inactiveTintColor: isDarkMode ? Color.white : Color.black,
+              tabBarStyle: {
+                backgroundColor: isDarkMode ? Color.darkTheme : Color.white,
+                borderTopColor: Color.Brown100,
+                borderTopWidth: 1,
+                height: Platform.OS === 'ios' ? '9.5%' : '7%',
+                position: 'absolute',
+                padding: Platform.OS === 'ios' ? '6%' : '6%',
               },
-            })}
-            options={{
-              tabBarLabel: '',
-              tabBarIcon: ({ focused }) => (
-                <View style={{ alignItems: 'center' }}>
-                  <MaterialCommunityIcons
-                    name={focused ? 'account' : 'account-outline'}
-                    style={{ marginTop: -10 }}
-                    color={isDarkMode ? Color.white : Color.darkTheme}
-                    size={26}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 10,
-                      color: isDarkMode ? Color.white : Color.black,
-                    }}
-                  >
-                    Profile
-                  </Text>
-                </View>
-              ),
             }}
-          />
-          <Tab.Screen
-            name="ChatStackScreen"
-            component={ChatStackScreen}
-            initialParams={{ isAuthenticated }}
-            listeners={({ navigation }) => ({
-              tabPress: (e) => {
-                e.preventDefault();
-                navigation.navigate('ChatStackScreen', {
-                  isAuthenticated,
-                });
-              },
-            })}
-            options={{
-              tabBarLabel: '',
-              tabBarIcon: ({ focused }) => (
-                <View style={{ alignItems: 'center' }}>
-                  <MaterialCommunityIcons
-                    name={focused ? 'chat' : 'chat-outline'}
-                    style={{ marginTop: -10 }}
-                    color={isDarkMode ? Color.white : Color.darkTheme}
-                    size={26}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 10,
-                      color: isDarkMode ? Color.white : Color.black,
-                    }}
-                  >
-                    Chats
-                  </Text>
-                </View>
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </PaperProvider>
-    </BottomSheetModalProvider>
+          >
+            <Tab.Screen
+              name="HomeStackScreen"
+              component={HomeStackScreen}
+              options={{
+                tabBarLabel: '',
+                tabBarIcon: ({ focused }) => (
+                  <View style={{ alignItems: 'center' }}>
+                    <MaterialCommunityIcons
+                      name={focused ? 'home' : 'home-outline'}
+                      style={{ marginTop: -10 }}
+                      color={isDarkMode ? Color.white : Color.darkTheme}
+                      size={26}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 10,
+                        color: isDarkMode ? Color.white : Color.black,
+                      }}
+                    >
+                      Home
+                    </Text>
+                  </View>
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="ProfileStackScreen"
+              component={ProfileStackScreen}
+              initialParams={{ isAuthenticated }}
+              listeners={({ navigation }) => ({
+                tabPress: (e) => {
+                  e.preventDefault();
+                  navigation.navigate('ProfileStackScreen', {
+                    isAuthenticated,
+                  });
+                },
+              })}
+              options={{
+                tabBarLabel: '',
+                tabBarIcon: ({ focused }) => (
+                  <View style={{ alignItems: 'center' }}>
+                    <MaterialCommunityIcons
+                      name={focused ? 'account' : 'account-outline'}
+                      style={{ marginTop: -10 }}
+                      color={isDarkMode ? Color.white : Color.darkTheme}
+                      size={26}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 10,
+                        color: isDarkMode ? Color.white : Color.black,
+                      }}
+                    >
+                      Profile
+                    </Text>
+                  </View>
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="ChatStackScreen"
+              component={ChatStackScreen}
+              initialParams={{ isAuthenticated }}
+              listeners={({ navigation }) => ({
+                tabPress: (e) => {
+                  e.preventDefault();
+                  navigation.navigate('ChatStackScreen', {
+                    isAuthenticated,
+                  });
+                },
+              })}
+              options={{
+                tabBarLabel: '',
+                tabBarIcon: ({ focused }) => (
+                  <View style={{ alignItems: 'center' }}>
+                    <MaterialCommunityIcons
+                      name={focused ? 'chat' : 'chat-outline'}
+                      style={{ marginTop: -10 }}
+                      color={isDarkMode ? Color.white : Color.darkTheme}
+                      size={26}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 10,
+                        color: isDarkMode ? Color.white : Color.black,
+                      }}
+                    >
+                      Chats
+                    </Text>
+                  </View>
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </PaperProvider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
 
