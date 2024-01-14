f02f4695bebc1a6560b93ee4fe99fa74c92ec305
amir fukman <amir.fukman@gmail.com>
2024-01-14 14:02:46 +0200

    merge Amir

--- screens/HouseDetailsScreen.js
+++ screens/HouseDetailsScreen.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import Animated, { SlideInDown } from "react-native-reanimated";
 import { SliderBox } from "react-native-image-slider-box";
 import HouseInfo from "../components/House/HouseInfo";
 import { Paragraph, Text } from "react-native-paper";
@@ -16,6 +17,7 @@ import HouseRoommates from "../components/House/HouseRoommates";
 import Seperator from "../components/Seperator";
 import HouseAssets from "../components/House/HouseAssets";
 import HouseAssetsModal from "../modals/HouseAssetsModal";
+import RoommatesInfo from "../components/House/RoommatesInfo";
 
 const DetailPage = ({ navigation, route }) => {
   const [mapPress, setMapPress] = useState(false);
@@ -72,6 +74,18 @@ const DetailPage = ({ navigation, route }) => {
     },
   ];
 
+  const Roommates = [
+    {
+      name: "Maor Saadia",
+      avatar_url: "../assets/images/profile-cartoon.png",
+      subtitle: "President",
+    },
+    {
+      name: "Amir Fukman",
+      avatar_url: "../assets/images/profile-cartoon.png",
+      subtitle: "Vice President",
+    },
+  ];
   const ParagraphDeatails =
     "Discover the perfect three-bedroom rental nestled in a tranquil suburban setting. This charming house features an open-concept living area with ample natural light, a modern kitchen, and a master bedroom with an en-suite bathroom. Enjoy the peaceful backyard with a patio and fire pit. Conveniently located near parks and shopping, this home offers both comfort and convenience for your lifestyle.";
 
@@ -85,36 +99,58 @@ const DetailPage = ({ navigation, route }) => {
   };
 
   return (
-    <ScrollView style={{ marginBottom: tabBarHeight }}>
-      <View style={styles.images}>
-        <SliderBox images={images} sliderBoxHeight={"100%"} />
-      </View>
-      <View style={styles.houseInfo}>
-        <Text style={styles.price}>1000$</Text>
-        <Text style={styles.city}>Beer Sheva</Text>
-        <Text style={styles.street}>Avigdor hameiri 21/3</Text>
-        <Text style={styles.distance}>2 kilometers away from SCE College</Text>
-        <HouseRoommates />
-        <Text style={styles.about}>About</Text>
-        <Paragraph>{ParagraphDeatails}</Paragraph>
-        <HouseInfo />
-        <Map
-          handleMapPress={handleMapPress}
-          zoomEnabled={false}
-          scrollEnabled={false}
-        />
-        {mapPress && <MapModal handleMapPress={handleMapPress} />}
-        <Seperator />
-        <HouseAssets handleShowAllPress={handleShowAllPress} Assets={Assets} />
-        {showAll && (
-          <HouseAssetsModal
-            handleMapPress={handleShowAllPress}
+    <View style={{ flex: 1 }}>
+      <Animated.ScrollView style={{ marginBottom: 10 }}>
+        <View style={styles.images}>
+          <SliderBox images={images} sliderBoxHeight={"100%"} />
+        </View>
+        <View style={styles.houseInfo}>
+          <Text style={styles.city}>Beer Sheva</Text>
+          <Text style={styles.street}>Avigdor hameiri 21/3</Text>
+          <Text style={styles.distance}>
+            2 kilometers away from SCE College
+          </Text>
+          <HouseRoommates />
+          <Text style={styles.about}>About</Text>
+          <Paragraph>{ParagraphDeatails}</Paragraph>
+          <HouseInfo />
+          <Map
+            handleMapPress={handleMapPress}
+            zoomEnabled={false}
+            scrollEnabled={false}
+          />
+          {mapPress && <MapModal handleMapPress={handleMapPress} />}
+          <Seperator />
+          <HouseAssets
+            handleShowAllPress={handleShowAllPress}
             Assets={Assets}
           />
-        )}
-        <Seperator />
-      </View>
-    </ScrollView>
+          {showAll && (
+            <HouseAssetsModal
+              handleMapPress={handleShowAllPress}
+              Assets={Assets}
+            />
+          )}
+          <Seperator />
+          <RoommatesInfo Roommates={Roommates} />
+        </View>
+      </Animated.ScrollView>
+
+      <Animated.View
+        style={[styles.BottomTab, { marginBottom: tabBarHeight }]}
+        entering={SlideInDown.delay(200)}
+      >
+        <View style={styles.BottomTabView}>
+          <View style={styles.PriceView}>
+            <Text style={styles.price}>1000$</Text>
+            <Text style={styles.monthPerson}>Month / Person</Text>
+          </View>
+          <TouchableOpacity style={styles.ReserveBtn}>
+            <Text style={styles.BtnText}>Reserve</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
+    </View>
   );
 };
 
@@ -130,11 +166,7 @@ const styles = StyleSheet.create({
     flexDirection: "col",
     marginHorizontal: "4%",
   },
-  price: {
-    fontSize: 35,
-    fontWeight: "bold",
-    color: "#65B741",
-  },
+
   city: {
     fontSize: 35,
     marginBottom: 5,
@@ -144,7 +176,6 @@ const styles = StyleSheet.create({
     fontSize: 25,
     marginBottom: 5,
     fontWeight: "bold",
-    // color: Color.gray,
   },
   distance: {
     fontSize: 20,
@@ -152,6 +183,49 @@ const styles = StyleSheet.create({
   },
   about: {
     fontSize: 25,
+    fontWeight: "bold",
+  },
+  footerText: {
+    height: "100%",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  footerPrice: {
+    fontSize: 18,
+  },
+  BottomTab: {
+    height: 60,
+    borderTopWidth: 1,
+    borderColor: "black",
+  },
+  BottomTabView: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  PriceView: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  price: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  monthPerson: {
+    marginHorizontal: 5,
+  },
+  ReserveBtn: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    backgroundColor: "#9ADE7B",
+    borderRadius: 10,
+  },
+  BtnText: {
+    color: "white",
     fontWeight: "bold",
   },
 });
