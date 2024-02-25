39de2debd5fc3cddc0fe6627d0f326f5d954c7af
amir fukman <amir.fukman@gmail.com>
2024-02-25 06:52:46 +0200

    home page adjustments

--- utils/http.js
+++ utils/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const BACKEND_URL = "http://192.168.1.246:3000/api/v1";
-const BACKEND_URL = "http://172.20.10.3:3000/api/v1";
-// const BACKEND_URL = "https://finder-rent-backend.vercel.app";
+// const BACKEND_URL = "http://172.20.10.3:3000/api/v1";
+const BACKEND_URL = "https://finder-rent-backend.vercel.app";
 
 // export function storeAllApartments(apartments) {
 //   axios.post(BACKEND_URL + "/apartments", data);
