eb6e009b6381fbfa9e3193cedfdb2d572cecd293
Maor <maorsa9@gmail.com>
2024-07-21 11:09:38 +0300

    more components unit test

---
+++ __test__/components/PageContainer.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import { Text } from "react-native-paper";
+import PageContainer from "../../components/PageContainer";
+
+describe("PageContainer", () => {
+  it("should render children correctly", () => {
+    const { getByText } = render(
+      <PageContainer>
+        <Text>Test Child</Text>
+      </PageContainer>
+    );
+
+    expect(getByText("Test Child")).toBeTruthy();
+  });
+});
